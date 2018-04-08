420f579044bfb71f945b6eea24e5bcc18e3fe8c3
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2018-04-08 18:23:50 -0500

    added the tom on... all of the other keys

--- scripts.js
+++ scripts.js
@@ -1,37 +1,46 @@
-window.addEventListener('keypress',playSound);
+window.addEventListener('keypress', playSound);
 
+window.addEventListener('load', (event) => {
+  console.log(event.target.getElementById('a'));
+})
 
 function playSound(event) {
   let instrumentElement = document.getElementById(event.key.toLowerCase());
-  if (instrumentElement){
+  if (instrumentElement) {
     instrumentElement.setAttribute('class', 'instrument-box selected-instrument');
-    setTimeout(() => {instrumentElement.setAttribute('class', 'instrument-box')},100);
-    let instrument;
-    switch (event.key.toLowerCase()) {
-      case 'a':
-        instrument = 'clap';
-        break;
-      case 's':
-        instrument = 'boom';
-        break;
-      case 'd':
-        instrument = 'hihat';
-        break;
-      case 'f':
-        instrument = 'kick';
-        break;
-      case 'j':
-        instrument = 'openhat';
-        break;
-      case 'k':
-        instrument = 'ride';
-        break;
-      case 'l':
-        instrument = 'snare';
-        break;
-      case ';':
-        instrument = 'tink';
-    }
-    new Audio(`sounds/${instrument}.wav`).play();
+    setTimeout(() => {
+      instrumentElement.setAttribute('class', 'instrument-box')
+    }, 100);
   }
+  let instrument;
+  switch (event.key.toLowerCase()) {
+    case 'a':
+      instrument = 'clap';
+      break;
+    case 's':
+      instrument = 'boom';
+      break;
+    case 'd':
+      instrument = 'hihat';
+      break;
+    case 'f':
+      instrument = 'kick';
+      break;
+    case 'j':
+      instrument = 'openhat';
+      break;
+    case 'k':
+      instrument = 'ride';
+      break;
+    case 'l':
+      instrument = 'snare';
+      break;
+    case ';':
+      instrument = 'tink';
+      break;
+    default:
+      instrument = 'tom';
+      break;
+  }
+  new Audio(`sounds/${instrument}.wav`).play();
 }
